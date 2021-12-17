4a369c204fbb635b3128f2a73e9c1acbe5792fd1
Vichingo455 <59311016+Vichingo455@users.noreply.github.com>
2021-12-17 18:50:51 +0100

    Update Disable Windows Defender.bat

--- Disable Windows Defender.bat
+++ Disable Windows Defender.bat
@@ -1,4 +1,11 @@
 @echo off
+net session >NUL 2>&1
+if %errorlevel% neq 0 (
+    echo Administrator privileges required!
+
+    pause
+    exit
+)
 reg add "HKLM\Software\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t REG_DWORD / d 1 /f
 echo Done! Restarting...
 wmic os where primary=1 reboot
