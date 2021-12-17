f60dcc97c1a9a291ff6ebd23f2188889d3b83508
Vichingo455 <59311016+Vichingo455@users.noreply.github.com>
2021-12-17 18:52:35 +0100

    Create Enable Windows Defender.bat

---
+++ Enable Windows Defender.bat
@@ -0,0 +1,10 @@
+@echo off
+net session >NUL 2>&1
+if %errorlevel% neq 0 ( 
+echo Administrator privileges required! 
+pause 
+exit
+)
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t REG_DWORD / d 0 /f
+echo Done! Restarting...
+wmic os where primary=1 rebootexit
